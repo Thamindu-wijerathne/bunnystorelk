bc1f024bedcb653f232e4f3b8a9ffec93e770e70
Thamindu Wijerathne <140165111+Thamindu-wijerathne@users.noreply.github.com>
2025-11-17 11:06:57 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'https://www.bunnystore.lk',
+          },
+        ],
+        destination: 'https://www.bunnystore.lk/shop',
+        permanent: true, // 301 redirect
+      },
+    ];
+  },
 };
 
 export default nextConfig;
